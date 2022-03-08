4b21c2a2f040756f8699556e9f1e717ff3239f00
Teemu Nasanen <teemu.nasanen@gmail.com>
2022-03-08 17:04:12 +0200

    feat: schema-based form validation with yup library

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,26 +1,48 @@
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 
-// Profile Form HTML + Bootstrap 
+// Profile Form HTML + Bootstrap + YUP 
+// (https://github.com/jquense/yup, https://react-hook-form.com/get-started, https://dev.to/franciscomendes10866/react-form-validation-with-react-hook-form-and-yup-4a98)
+//
 // Todo:
-//      Validation
-//      Errors
-//      Submit...
+//      Validation: OK
+//      Errors: Need custom messages
+//      Submit
 
-const requiredConfig = {
-  required: true,
-  minLength: 2,
-};
+const schema = yup.object({
+  first_name: yup.string().max(20).required(),
+  last_name: yup.string().max(20).required(),
+  address_line_1: yup.string().max(100).required(),
+  address_line_2: yup.string().max(100).required(),
+  address_line_3: yup.string().max(100),
+  city: yup.string().max(60).required(),
+  country: yup.string().max(60).required(),
+  postal_code: yup.string().max(10).required(),
+  email: yup.string().email().max(100).required(),
+  height: yup.number().positive().required(),
+  weight: yup.number().positive().required(),
+  medical_conditions: yup.string().max(255),
+  disabilities: yup.string().max(255),
+  checkInformation: yup.boolean().required(),
+  requestForContributor: yup.boolean(),
+
+});
 
 const ProfileForm = () => {
 
-  const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
-
+  const { register, handleSubmit, formState:{ errors }, reset } = useForm({
+    resolver: yupResolver(schema)
+  });
+  const onSubmit = (data) => {
+    console.log({ data });
+    reset();
+  };
   
 
   return (
@@ -33,44 +55,49 @@ const ProfileForm = () => {
             <Col>
               <Form.Group className="mb-3" controlId="firstName">
                 <Form.Label>First Name</Form.Label>
-                <Form.Control {...register("firstName", requiredConfig)}
+                <Form.Control {...register("first_name")}
                   type="text"
                   placeholder="Enter first name"
                 ></Form.Control>
+                <p>{errors.first_name?.message}</p>
               </Form.Group>
             </Col>
             <Col>
               <Form.Group className="mb-3" controlId="lastName">
                 <Form.Label>Last Name</Form.Label>
-                <Form.Control  {...register("lastName", requiredConfig)}
+                <Form.Control  {...register("last_name")}
                   type="text"
                   placeholder="Enter last name"
                 ></Form.Control>
+                <p>{errors.last_name?.message}</p>
               </Form.Group>
             </Col>
           </Row>
           <Form.Group  className="mb-3" controlId="address1">
             <Form.Label>Address</Form.Label>
-            <Form.Control  {...register("address1", requiredConfig)} type="text" placeholder="Street"></Form.Control>
+            <Form.Control  {...register("address_line_1")} type="text" placeholder="Street"></Form.Control>
+            <p>{errors.address_line_1?.message}</p>
           </Form.Group>
           <Row>
             <Col>
               <Form.Group  className="mb-3" controlId="address2">
                 <Form.Label>Address 2</Form.Label>
-                <Form.Control  {...register("address2", requiredConfig)}
+                <Form.Control  {...register("address_line_2")}
                   type="text"
                   placeholder="House, apartment, floor..."
                 ></Form.Control>
+                <p>{errors.address_line_2?.message}</p>
               </Form.Group>
             </Col>
             <Col>
               <Form.Group className="mb-3" controlId="address3">
                 <Form.Label>Address 3</Form.Label>
-                <Form.Control {...register("address3")}
+                <Form.Control {...register("address_line_3")}
                   type="text"
                   placeholder="
                 If necessary, additional address information"
                 ></Form.Control>
+                <p>{errors.address_line_3?.message}</p>
               </Form.Group>
             </Col>
           </Row>
@@ -78,19 +105,21 @@ const ProfileForm = () => {
             <Col>
               <Form.Group className="mb-3" controlId="postalCode">
                 <Form.Label>Postal Code</Form.Label>
-                <Form.Control  {...register("postalCode", requiredConfig)}
+                <Form.Control  {...register("postal_code")}
                   type="text"
                   placeholder="Enter postal code"
                 ></Form.Control>
+                <p>{errors.postal_code?.message}</p>
               </Form.Group>
             </Col>
             <Col>
               <Form.Group className="mb-3" controlId="city">
                 <Form.Label>City</Form.Label>
-                <Form.Control {...register("city", requiredConfig)}
+                <Form.Control {...register("city")}
                   type="text"
                   placeholder="Enter city"
                 ></Form.Control>
+                <p>{errors.city?.message}</p>
               </Form.Group>
             </Col>
           </Row>
@@ -98,19 +127,21 @@ const ProfileForm = () => {
             <Col>
               <Form.Group className="mb-3" controlId="country">
                 <Form.Label>Country</Form.Label>
-                <Form.Control {...register("country", requiredConfig)}
+                <Form.Control {...register("country")}
                   type="text"
                   placeholder="Enter country"
                 ></Form.Control>
+                <p>{errors.country?.message}</p>
               </Form.Group>
             </Col>
             <Col>
               <Form.Group className="mb-3" controlId="email">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control {...register("email", requiredConfig)}
+                <Form.Control {...register("email")}
                   type="email"
                   placeholder="Enter email"
                 ></Form.Control>
+                <p>{errors.email?.message}</p>
               </Form.Group>
             </Col>
           </Row>
@@ -119,30 +150,33 @@ const ProfileForm = () => {
             <Col>
               <Form.Group className="mb-3" controlId="height">
                 <Form.Label>Height in cm</Form.Label>
-                <Form.Control {...register("height", requiredConfig)}
+                <Form.Control {...register("height")}
                   type="number"
                   placeholder="Enter height in centimeters: 180.5"
                   defaultValue={150}
                 ></Form.Control>
+                <p>{errors.height?.message}</p>
               </Form.Group>
             </Col>
             <Col>
               <Form.Group className="mb-3" controlId="weight">
                 <Form.Label>Weight in kg</Form.Label>
-                <Form.Control {...register("weight", requiredConfig)}
+                <Form.Control {...register("weight")}
                   type="number"
                   placeholder="Enter weight in kilograms: 70.5"
                   defaultValue={60}
                 ></Form.Control>
+                <p>{errors.weight?.message}</p>
               </Form.Group>
             </Col>
           </Row>
           <Form.Group className="mb-3" controlId="medicalConditions">
             <Form.Label>Medical Conditions</Form.Label>
-            <Form.Control {...register("medicalConditions")}
+            <Form.Control {...register("medical_conditions")}
               as="textarea"
               placeholder="If you have any medical condition that needs attention, please let us know "
             ></Form.Control>
+            <p>{errors.medical_conditions?.message}</p>
           </Form.Group>
           <Form.Group className="mb-3" controlId="disabilities">
             <Form.Label>Disabilities</Form.Label>
@@ -150,6 +184,7 @@ const ProfileForm = () => {
               as="textarea"
               placeholder="If you have any disabilities, please let us know"
             ></Form.Control>
+            <p>{errors.disabilities?.message}</p>
           </Form.Group>
           <hr />
           <Row className="mb-5">
@@ -160,6 +195,7 @@ const ProfileForm = () => {
                   label="I have filled in and checked that the information is correct"
                   required
                 />
+                <p>{errors.checkInformation?.message}</p>
               </Form.Group>
             </Col>
             <Col>
@@ -171,6 +207,7 @@ const ProfileForm = () => {
                   type="checkbox"
                   label="I request Contributor status"
                 />
+                <p>{errors.requestForContributor?.message}</p>
               </Form.Group>
             </Col>
           </Row>
