1b93a6c663dd0f779ed670b623c6cfb3851ee0e4
Teemu Nasanen <teemu.nasanen@gmail.com>
2022-03-16 11:20:35 +0200

    fix: rename login view to home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 
-const Login = () => {
+const Home = () => {
   return (
     <>
-    Login Page
+    Home Page
     </>
   );
 };
 
-export default Login;
+export default Home;
