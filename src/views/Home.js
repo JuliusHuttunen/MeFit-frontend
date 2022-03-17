90c768666dc27dc2d667889de555507073993e2e
Teemu Nasanen <teemu.nasanen@gmail.com>
2022-03-17 15:52:15 +0200

    fix: home page white body

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import CarouselHome from "../components/carousel/CarouselHome";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <>
+    <div className={styles}>
       <CarouselHome />
-    </>
+    </div>
   );
 };
 
